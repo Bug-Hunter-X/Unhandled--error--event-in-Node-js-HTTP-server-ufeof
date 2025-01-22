89d3484880301eda12f7ae6b4f5dd685176e7252
BugHunter-X <askzenai@gmail.com>
2025-01-23 00:44:42 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+server.on('error', (err) => {
+  console.error('Server error:', err);
+});
+
+server.listen(8080);
+console.log('Server is running on port 8080');
